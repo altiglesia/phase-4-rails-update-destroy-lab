9fa6d819ad6b1a7fd27119d2654ead4112070693
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2022-11-26 12:49:13 -0500

    commit tree 7c88c0038963ac2965d1d978ae835a2d2e448680

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -8,7 +8,7 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
     render json: plant
   end
 
@@ -18,9 +18,40 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  # PATCH /plants/:id
+  def update
+    plant = find_plant
+    if plant
+      plant.update(plant_params)
+      render json: plant
+    else
+      render_not_found_response
+    end
+  end
+
+  # DELETE /plants/:id
+  def destroy
+    plant = find_plant
+    if plant
+      plant.destroy
+      head :no_content
+    else
+      render_not_found_response
+    end
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+
+  def find_plant
+    plant = Plant.find_by(id: params[:id])
+  end
+
+  def render_not_found_response
+    render json: { error: "Plant not found" }, status: :not_found
+  end
+
 end
